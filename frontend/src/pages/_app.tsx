60eced18b9823cc134c6659c241c5c8b8dc39385
PK-Yakkun <mokkun515@gmail.com>
2023-02-25 11:13:10 +0900

    feat: next/fontにてNotoSerifJPをグローバルに適応

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Noto_Serif_JP } from "@next/font/google";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const notoSerifJp = Noto_Serif_JP({
+  weight: ["700", "500"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap", // ここが大事
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${notoSerifJp.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
