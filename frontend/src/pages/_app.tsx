81cadd1968cf75109f6bd68a6a53f4cb5962ab68
PK-Yakkun <mokkun515@gmail.com>
2023-02-26 23:17:58 +0900

    feat: noto serif JPの太さ追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { theme } from "@/styles/theme";
 import "@/styles/globals.css";
 
 const notoSerifJp = Noto_Serif_JP({
-  weight: ["700", "500"],
+  weight: ["700", "500", "400"],
   style: ["normal"],
   subsets: ["latin"],
   display: "swap", // ここが大事
