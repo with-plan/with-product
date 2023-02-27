d3a630d09f2b47d56df9af1aac9ba2f772634af7
PK-Yakkun <mokkun515@gmail.com>
2023-02-27 21:24:34 +0900

    feat: Typography改修

--- frontend/src/components/atoms/Typography/index.tsx
+++ frontend/src/components/atoms/Typography/index.tsx
@@ -1,25 +1,41 @@
 import { Box } from "theme-ui";
 
 interface Typography {
-  as?: "h1" | "h2" | "h3" | "h4" | "p";
+  as?: "h1" | "h2" | "h3" | "p";
+  color?: string;
   children?: React.ReactNode;
 }
 
+interface GenerateFontStyles {
+  as?: string;
+  color?: string;
+}
+
 export function Typography(props: Typography) {
-  const generateFontStyles = (as: string | undefined) => {
+  const generateFontStyles = ({ as = "p", color = "typography" }) => {
+    let style = {};
     switch (as) {
       case "h1":
-        return { fontSize: ["76px"], fontWeight: "bold" };
+        style = { ...style, fontSize: ["76px"], fontWeight: "bold" };
       case "h2":
-        return { fontSize: ["28px"], fontWeight: "bold" };
+        style = { ...style, fontSize: ["28px"], fontWeight: "bold" };
       case "h3":
-        return { fontSize: ["22px"], fontWeight: "bold" };
+        style = { ...style, fontSize: ["22px"], fontWeight: "bold" };
       case "p":
-        return { fontSize: ["20px"], fontWeight: "400", lineHeight: "1.85" };
+        style = {
+          ...style,
+          fontSize: ["20px"],
+          fontWeight: "400",
+          lineHeight: "1.85",
+        };
     }
   };
   return (
-    <Box as={props.as} sx={generateFontStyles(props.as)}>
+    <Box
+      as={props.as}
+      style={{ color: "#111" }}
+      sx={generateFontStyles(props.as)}
+    >
       {props.children}
     </Box>
   );
