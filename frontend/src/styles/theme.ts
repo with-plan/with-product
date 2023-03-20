3a3a9a1703b4b7edcdcfa2004ab66adba1f2d8bd
PK-Yakkun <mokkun515@gmail.com>
2023-03-20 22:41:21 +0900

    feat: themeにcolor追加、space設定

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -15,7 +15,9 @@ export const theme: Theme = {
     accent: {
       primary: "#e3e0da",
       secondary: "#ccba94",
+      tertiary: "#afafaf",
     },
     link: "#508cb2",
   },
+  space: [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60],
 };
