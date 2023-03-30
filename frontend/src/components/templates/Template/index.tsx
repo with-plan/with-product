b1a7e48efd108969a0b43fa55f8954d726f636ff
PK-Yakkun <mokkun515@gmail.com>
2023-03-30 11:39:12 +0900

    feat: Template改修

--- frontend/src/components/templates/Template/index.tsx
+++ frontend/src/components/templates/Template/index.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import { Typography } from "@/components/atoms/Typography";
 import { Breadcrumb } from "@/components/organisms/Breadcrumb";
 import { Header } from "@/components/organisms/Header";
-import router from "next/router";
 import { Box } from "theme-ui";
 
 interface TemplateProps {
@@ -12,13 +10,32 @@ interface TemplateProps {
 
 export function Template({ pageName, contents }: TemplateProps) {
   return (
-    <Box sx={{ height: "100vh", background: "base.secondary" }}>
+    <Box
+      sx={{
+        position: "relative",
+        height: "100vh",
+        background: "base.secondary",
+        "::before": {
+          content: '""',
+          display: "block",
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          width: ["375px", "678px"],
+          height: ["144px", "516px"],
+          background: [
+            "url(svg/line_curve_sp.svg) no-repeat",
+            "url(svg/line_curve_pc.svg) no-repeat",
+          ],
+        },
+      }}
+    >
       <Header />
       <Box sx={{ py: 3 }}>
         <Breadcrumb label={pageName} />
       </Box>
 
-      <Box as="section">
+      <Box as="section" sx={{ px: [3, "0px"] }}>
         <Box sx={{ maxWidth: "1000px", pt: 10, mx: "auto", flex: 1 }}>
           <Typography as="h2">{pageName}</Typography>
           {contents}
