c8afbdc4512b8dd0c289867e037173727805f9c5
PK-Yakkun <mokkun515@gmail.com>
2023-03-31 14:08:45 +0900

    feat: metaの設定

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import { theme } from "@/styles/theme";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const notoSerifJp = Noto_Serif_JP({
   weight: ["700", "500", "400"],
@@ -13,15 +14,26 @@ const notoSerifJp = Noto_Serif_JP({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <style jsx global>{`
-        html {
-          font-family: ${notoSerifJp.style.fontFamily};
-          color: #111;
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>With Product</title>
+        <meta
+          name="description"
+          content={
+            "Width ProductはWith企画が運営する古物商の事業会社です。高品質な商品のみを扱い、お客様の満足度を第一に掲げて日々活動しております。"
+          }
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <style jsx global>{`
+          html {
+            font-family: ${notoSerifJp.style.fontFamily};
+            color: #111;
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
